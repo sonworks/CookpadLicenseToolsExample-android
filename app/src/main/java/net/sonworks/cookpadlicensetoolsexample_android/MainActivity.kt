ea8a803191081c77d60f1d65756b3ff6fc7752ab
sonworks <csw0413@outlook.com>
2019-04-23 00:31:44 +0900

    Add Licenses page

--- app/src/main/java/net/sonworks/cookpadlicensetoolsexample_android/MainActivity.kt
+++ app/src/main/java/net/sonworks/cookpadlicensetoolsexample_android/MainActivity.kt
@@ -1,12 +1,14 @@
 package net.sonworks.cookpadlicensetoolsexample_android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView.loadUrl("file:///android_asset/licenses.html")
     }
 }
